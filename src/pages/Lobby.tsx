005eddfd16b5adaba699380887176a9f68ac5b24
nitaysc <7nitay7@gmail.com>
2026-04-27 14:45:11 +0300

    Add keno game page and lobby card

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -22,7 +22,8 @@ import snakesImg from "@/assets/games/snakes.jpg";
 import molesImg from "@/assets/games/moles.jpg";
 import chessImg from "@/assets/games/chess.jpg";
 import pumpImg from "@/assets/games/pump.jpg";
- import rouletteImg from "@/assets/games/roulette.png";
+import rouletteImg from "@/assets/games/roulette.png";
+import kenoImg from "@/assets/games/keno.svg";
 
 export default function Lobby() {
   const { profile, refetch } = useUserProfile();
@@ -58,6 +59,7 @@ export default function Lobby() {
     { to: "/chess", key: "chess", title: "CHESS", img: chessImg },
     { to: "/pump", key: "pump", title: "PUMP", img: pumpImg },
     { to: "/roulette", key: "roulette", title: "ROULETTE", img: rouletteImg },
+    { to: "/keno", key: "keno", title: "KENO", img: kenoImg },
   ];
 
   return (
